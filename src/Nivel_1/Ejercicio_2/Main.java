67de2ec4b3a40f13dd4a26308ca0284ef7997dbf
Fer_ <fg.roglia@gmail.com>
2024-11-28 11:33:24 +0100

    Ejercicio_1_Nivel_1_Finalizado

---
+++ src/Nivel_1/Ejercicio_2/Main.java
@@ -0,0 +1,33 @@
+package Nivel_1.Ejercicio_2;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println("\n-\n");
+
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+
+        ListIterator<Integer> it = numbers.listIterator(numbers.size());
+        while (it.hasPrevious()) {
+            int number = it.previous();
+            numbers2.add(number);
+            System.out.println(number);
+        }
+    }
+}
